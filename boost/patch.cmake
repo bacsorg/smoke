7ee81862ad64b3fc9e7610453bb60e5995cd2015
Aleksey Filippov <sarum9in@gmail.com>
2015-08-30 00:50:13 +0400

    Fixed python configuration in boost.

--- boost/patch.cmake
+++ boost/patch.cmake
@@ -1,10 +1,13 @@
+if(WIN32)
+    file(APPEND ${BOOST_SOURCE_ROOT}/project-config.jam
+         "using python : "
+         "\"${BUNSAN_SMOKE_PYTHON2_VERSION}\" : "
+         "\"${BUNSAN_SMOKE_PYTHON2_PREFIX}\" : "
+         "\"${BUNSAN_SMOKE_PYTHON2_INCLUDE_DIR}\" : "
+         "\"${BUNSAN_SMOKE_PYTHON2_LIBRARY_DIR}\" ;\n"
+    )
+endif()
 file(APPEND ${BOOST_SOURCE_ROOT}/project-config.jam
-     "using python : "
-     "\"${BUNSAN_SMOKE_PYTHON2_VERSION}\" : "
-     "\"${BUNSAN_SMOKE_PYTHON2_PREFIX}\" : "
-     "\"${BUNSAN_SMOKE_PYTHON2_INCLUDE_DIR}\" : "
-     "\"${BUNSAN_SMOKE_PYTHON2_LIBRARY_DIR}\" ;\n"
-
      "using python : "
      "\"${BUNSAN_SMOKE_PYTHON3_VERSION}\" : "
      "\"${BUNSAN_SMOKE_PYTHON3_PREFIX}\" : "
