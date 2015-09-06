c342c79d2678430933491c196c3378084f148aa7
Aleksey Filippov <sarum9in@gmail.com>
2015-09-06 04:46:54 +0400

    Fixed grpc Makefile.

--- grpc/patch.cmake
+++ grpc/patch.cmake
@@ -9,6 +9,8 @@ file(WRITE ${GRPC_SOURCE_ROOT}/src/core/support/string_win32.c "${STRING_WIN32_C
 
 file(READ ${GRPC_SOURCE_ROOT}/Makefile MAKEFILE)
 string(REPLACE "-Werror" "" MAKEFILE "${MAKEFILE}")
+string(REPLACE "LIBS = m z pthread" "LIBS = m zlib pthread" MAKEFILE "${MAKEFILE}")
+string(REPLACE "/gpr.$(SHARED_EXT)" "/libgpr.$(SHARED_EXT)" MAKEFILE "${MAKEFILE}")
 file(WRITE ${GRPC_SOURCE_ROOT}/Makefile "${MAKEFILE}")
 
 file(READ ${GRPC_SOURCE_ROOT}/include/grpc/support/port_platform.h PORT_PLATFORM_H)
