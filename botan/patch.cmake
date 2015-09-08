759688257c6b9cc48b27e5f5abacc74ea14d1b10
Aleksey Filippov <sarum9in@gmail.com>
2015-09-08 13:08:00 +0400

    Fixed botan patch.

--- botan/patch.cmake
+++ botan/patch.cmake
@@ -1,5 +1,5 @@
 file(APPEND ${BOTAN_SOURCE_ROOT}/src/build-data/os/mingw.txt  "build_shared yes\n")
 
 file(READ ${BOTAN_SOURCE_ROOT}/src/scripts/install.py INSTALL_PY)
-string(REPLACE "os.symlink" "shutil.copyfile" INSTALL_PY ${INSTALL_PY})
-file(WRITE ${BOTAN_SOURCE_ROOT}/src/scripts/install.py ${INSTALL_PY})
+string(REPLACE "os.symlink" "shutil.copyfile" INSTALL_PY "${INSTALL_PY}")
+file(WRITE ${BOTAN_SOURCE_ROOT}/src/scripts/install.py "${INSTALL_PY}")
